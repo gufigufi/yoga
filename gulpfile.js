d07a20c98ca7fa8dfae51c3460c749b1151a677b
gufigufi <gufigufi.42@gmail.com>
2018-12-10 22:01:45 +0300

    structure
    creating project structure

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCss    = require('gulp-minify-css'),
+    rename       = require('gulp-rename'),
+    connect      = require('gulp-connect'),
+    livereload   = require('gulp-livereload'),
+    wiredep      = require('wiredep').stream,
+    useref       = require('gulp-useref'),
+    gulpif       = require('gulp-if'),
+    uglify       = require('gulp-uglify'),
+    clean       = require('gulp-clean');
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        livereload: true
+    })
+});
+
+gulp.task('bower', function () {
+    gulp.src('./app/index.html')
+        .pipe(wiredep({
+            directory: "app/bower_components"
+        }))
+        .pipe(gulp.dest('./app'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('app/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(connect.reload())
+});
+gulp.task('html', function () {
+    gulp.src('app/*.html')
+    .pipe(connect.reload())
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/**/*.sass', ['sass'])
+    gulp.watch('app/*.html', ['html'])
+    gulp.watch('bower.json', ['bower'])
+});
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean({force: true}));
+});
+gulp.task('build', ['clean'], function () {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['connect', 'watch']);
