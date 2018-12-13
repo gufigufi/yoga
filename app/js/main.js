951e0370a44369b7302372f107e5b9dfa1aab192
gufigufi <gufigufi.42@gmail.com>
2018-12-13 20:49:45 +0300

    popup
    popup price form

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,25 @@ $('.aboutUs__slider').slick({
     prevArrow: '<div class="aboutUs__prew"><img src="../img/icons/arrow.png" alt=""></div>',
     nextArrow: '<div class="aboutUs__next"><img src="../img/icons/arrow.png" alt=""></div>'
 });
+
+//blocking scroll the main block
+document.onwheel = function(e) {
+    if (e.target.id != 'popUp') return;
+    var area = e.target;
+    var delta = e.deltaY || e.detail || e.wheelDelta;
+    if (delta < 0 && area.scrollTop == 0) {
+        e.preventDefault();
+    }
+    if (delta > 0 && area.scrollHeight - area.clientHeight - area.scrollTop <= 1) {
+        e.preventDefault();
+    }
+};
+$('.prise__buy').click(function(){
+    $('#popUp').addClass('active');
+});
+$('#closePopUp').click(function () {
+    $('#popUp').removeClass('active')
+});
+$('#popUp').click(function () {
+    $(this).removeClass('active')
+});
