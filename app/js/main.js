4fd35c8e3796d49c9b82a69c7286fd14670bc200
gufigufi <gufigufi.42@gmail.com>
2018-12-13 14:56:03 +0300

    auoutUs
    block about us end slider

---
+++ app/js/main.js
@@ -0,0 +1,5 @@
+$('.aboutUs__slider').slick({
+    infinite: true,
+    prevArrow: '<div class="aboutUs__prew"><img src="../img/icons/arrow.png" alt=""></div>',
+    nextArrow: '<div class="aboutUs__next"><img src="../img/icons/arrow.png" alt=""></div>'
+});
